89e72039013d3aeaaeaaf640b79493ba47cdfc6e
pritesh-ugrankar <pritesh-manohar-ugrankar@gmail.com>
2018-01-29 02:03:55 +0530

    Added comments to 2nd finger exercise

--- finger_ex2_largest_odd.py
+++ finger_ex2_largest_odd.py
@@ -1,3 +1,7 @@
+#Write a program that examines three variables–x, y, and z 
+#and prints the largest odd number among them. If none of them are odd, 
+#it should print a message to that effect.
+
 x = int( input ( "Enter x: " ) )
 y = int( input ( "Enter y: " ) )
 z = int( input ( "Enter z: " ) )
