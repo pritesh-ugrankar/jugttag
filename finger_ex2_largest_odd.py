484a95a7761fe990c55dc81b7b09e272dab13ae5
pritesh-ugrankar <pritesh-manohar-ugrankar@gmail.com>
2018-01-29 01:04:41 +0530

    Uploading the exercises in John Guttag's book

---
+++ finger_ex2_largest_odd.py
@@ -0,0 +1,42 @@
+x = int( input ( "Enter x: " ) )
+y = int( input ( "Enter y: " ) )
+z = int( input ( "Enter z: " ) )
+
+if x % 2 == 0 and y % 2 == 0 and z % 2 == 0:
+    print ("No odd numbers")
+elif x % 2 == 0 and y % 2 == 0 and z % 2 != 0:
+    print ("z: ", z , " is the greatest odd number amongst", x, y, z)
+elif x % 2 == 0 and y % 2 != 0 and z % 2 == 0:
+    print ("y: ", y , " is the greatest odd number amongst", x, y, z)
+elif x % 2 != 0 and y % 2 == 0 and z % 2 == 0:
+    print ("x: ", x, " is the greatest odd number amongst", x, y, z)
+
+
+elif x % 2 == 0 and y % 2 != 0 and z % 2 !=0:
+    if y > z:
+        print ("y:", y , "is the greatest odd number amongst", x, y, z)
+    else:
+        print ("z:", z , "is the greatest odd number amongst", x, y, z)
+        
+
+elif x % 2 != 0 and y % 2 != 0 and z % 2 == 0:
+    if x > y:
+        print ("x:", x , "is the greatest odd number amongst", x, y, z)
+    else:
+        print ("y:", y , "is the greatest odd number amongst", x, y, z)
+
+elif x % 2 != 0 and y % 2 == 0 and z % 2 != 0:
+    if x > z:
+        print ("x:", x , "is the greatest odd number amongst", x, y, z)
+    else:
+        print ("z:", z , "is the greatest odd number amongst", x, y, z)
+
+elif x % 2 != 0 and y % 2 != 0 and z %2 != 0:
+    if x > y and x > z:
+        print ("x:", x , "is the greatest odd number amongst", x, y, z)
+    elif y > z:
+        print ("y:", y , "is the greatest odd number amongst", x, y, z)
+    else:
+        print ("z:", z , "is the greatest odd number amongst", x, y, z)
+
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+For pushing the exercises from John Guttag's "Introduction to Computation and Programming Using Python with Application to Understanding Data" Book.
